bf781e2f413970223388b69402b79acec04b2efd
abhsihek <abhishekshakya98.com@gmail.com>
2024-02-05 12:28:53 +0545

    Added which was overrided on merge

---
+++ src/entities/bookmark/constant/bookmarkExceptionMessages.ts
@@ -0,0 +1,10 @@
+export const bookmarkExceptionMessages = {
+    TITLE_FOLDER_REQUIRED: 'Title and folder ID are required.',
+    INVALID_ID: 'Invalid bookmark ID.',
+    TITLE_REQUIRED: 'Title is required.',
+    BOOKMARK_NOT_FOUND: 'Bookmark does not exist.',
+    BOOKMARK_EMPTY: 'No bookmarks found.',
+    ADD_FAILED: 'Failed to add bookmark.',
+    UPDATE_FAILED: 'Failed to update bookmark.',
+    DELETE_FAILED: 'Failed to delete bookmark.',
+}
--- src/entities/folder/folder.routes.ts
+++ src/entities/folder/folder.routes.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { deleteFolders, getAllFolders, patchFolders, postFolders } from './folder.controller';
 import { verifyToken } from '../../auth/middleware/authentication.middleware';
 import multer from 'multer';
+import joiValidationMiddleware from '../../auth/middleware/joiValidationMiddleware';
+import folderSchema from './folder.schema';
 
 const router = express.Router();
 
@@ -9,7 +11,7 @@ const upload = multer({ dest: 'temp/' });
 
 router.use(verifyToken)
 
-router.get('/', getAllFolders).post('/', upload.single('litmark_image'), verifyToken, postFolders)
+router.get('/', getAllFolders).post('/', upload.single('litmark_image'), joiValidationMiddleware(folderSchema), verifyToken, postFolders)
 router.patch('/:id', upload.single('litmark_image'), verifyToken, patchFolders).delete('/:id', deleteFolders)
 
 export default router;
