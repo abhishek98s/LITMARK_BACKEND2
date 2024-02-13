360a4846367983bdbf3e285b4238d653be342826
abhishek98s <abhishekshakya98.com@gmail.com>
2024-02-13 16:21:50 +0545

    09-SwaggerDocumentation

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -1,7 +1,24 @@
-import express from 'express';
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocumentation from './swagger_output.json'
+import swaggerJSDoc from 'swagger-jsdoc'
 
-export const swagger = function (app: express.Application) {
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocumentation));
-}
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Your API Title',
+      version: '1.0.0',
+      description: 'API documentation for your Node.js application',
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000/api',
+      },
+    ],
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: [`${__dirname}/../**/*.ts`], // Path to the API routes
+};
+export const swaggerSpec = swaggerJSDoc(options);
