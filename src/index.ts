f8425399e0ec58c65898c2b01719f2a34f0fd69d
abhsihek <abhishekshakya98.com@gmail.com>
2024-02-20 11:51:37 +0545

    added cors to prevent the cors issue

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import express from 'express'
 import {config} from './config/config';
 import { swagger } from './docs/swagger';
+import cors from 'cors';
 
 const app = express()
 const port = config.app.port
 const name = config.app.name
 app.use(express.json())
+app.use(cors());
 
 import imageRoutes from './entities/image/image.routes'
+import userRoutes from './entities/user/user.routes'
+import chipRoutes from './entities/chip/chip.routes'
+import folderRoutes from './entities/folder/folder.routes'
+import authRoutes from './auth/routes/auth.routes'
 
 import { logMiddleware } from './logger/logger';
 
