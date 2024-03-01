7681f90399471994cf04fc043961d76453b5c0b5
abhsihek <abhishekshakya98.com@gmail.com>
2024-03-01 18:34:19 +0545

    Refactor code

--- src/entities/folder/folder.schema.ts
+++ src/entities/folder/folder.schema.ts
@@ -3,7 +3,6 @@ import joi, { Schema } from 'joi';
 const folderSchema: Schema = joi.object().keys({
     name: joi.string().required(),
     image_id: joi.number().required(),
-    user_id: joi.number().required(),
     folder_id: joi.number().required(),
     user: joi.object().keys({
         id: joi.number().required(),
