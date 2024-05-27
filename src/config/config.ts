68e3a33e7c9c6bdfd42cc144e1e1d05ebd6bc916
abhsihek <abhishekshakya98.com@gmail.com>
2024-05-27 12:43:49 +0545

    initial commit

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,9 +5,9 @@ dotenv.config()
 export const config = {
     app: {
         name: process.env.NAME || 'LITMARK_BACKEND',
-        port: process.env.SERVER_PORT || '5000'
+        port: process.env.SERVER_PORT || '5000',
     },
-    database: getActiveDatabase(process.env.ACTIVE_DB || 'mysql2')
+    database: getActiveDatabase(process.env.ACTIVE_DB || 'mysql2'),
 }
 
 function getActiveDatabase(db: string) {
@@ -20,7 +20,19 @@ function getActiveDatabase(db: string) {
                 database: process.env.DB_NAME,
                 host: process.env.DB_HOST || '127.0.0.1',
                 port: parseInt(process.env.DB_PORT!) || 3306,
-            }
+            },
+        };
+    } else if (db === 'pg') {
+        return {
+            client: db,
+            connection: {
+                connectionString: 'ep-dawn-wind-42752734-pooler.us-east-1.aws.neon.tech',
+                user: 'DB_USER',
+                password: 'DB_PASSWORD',
+                database: 'DB_NAME',
+                host: 'DB_HOST',
+                port: 'DB_PORT',
+            },
         };
     }
 }
