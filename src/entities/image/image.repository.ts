7f1c12327a875a299de4a545b7d77f32f6d3e64f
abhsihek <abhishek_mi@yonefu.info>
2024-05-29 16:38:14 +0545

    docs of image

--- src/entities/image/image.repository.ts
+++ src/entities/image/image.repository.ts
@@ -32,10 +32,28 @@ export const create = async (imageData: ImageModel) => {
     return { image_id: id };
 }
 
+/**
+ * The function `update` updates an image record in the database with the provided image data based on
+ * the image ID.
+ * @param {ImageModel} imageData - The `imageData` parameter is of type `ImageModel`, which likely
+ * contains the data representing an image such as its URL, dimensions, and other relevant information.
+ * @param {number} imageId - The `imageId` parameter is the unique identifier of the image in the
+ * database that you want to update.
+ * @returns The `update` function is returning a promise that resolves to the result of updating the
+ * `images` table in the database where the `id` matches the `imageId` with the provided `imageData`.
+ */
 export const update = async (imageData: ImageModel, imageId: number) => {
     return await knex('images').where('id', imageId).update(imageData);
 }
 
+/**
+ * The function `remove` updates the `isdeleted` field to true for a specific image in the database
+ * using the imageId.
+ * @param {number} imageId - The `imageId` parameter is a number that represents the unique identifier
+ * of the image that you want to mark as deleted in the database.
+ * @returns The `remove` function is returning a promise that updates the `isdeleted` column in the
+ * `images` table to `true` for the image with the specified `imageId`.
+ */
 export const remove = async (imageId: number) => {
     return await knex('images').update('isdeleted', true).where('id', imageId);
 }
