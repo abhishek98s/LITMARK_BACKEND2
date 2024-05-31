c96ac02bfd830ee93976726c2790d165297e7cb9
abhsihek <abhishek_mi@yonefu.info>
2024-05-31 12:01:06 +0545

    addded vercel

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,7 @@ dotenv.config()
 
 export const config = {
     app: {
-        name: process.env.NAME || 'LITMARK_BACKEND',
+        name: 'LITMARK_BACKEND',
         port: process.env.SERVER_PORT || '5000'
     },
     database: getActiveDatabase(process.env.ACTIVE_DB || 'mysql2')
