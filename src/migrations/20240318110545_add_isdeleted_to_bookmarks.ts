c90a2eb3b7f952ebf3a316007c3e4c298eef653b
abhishek98s <abhishekshakya98.com@gmail.com>
2025-04-02 15:59:29 +0545

    fix: check of table eixits or not before altering

--- src/migrations/20240318110545_add_isdeleted_to_bookmarks.ts
+++ src/migrations/20240318110545_add_isdeleted_to_bookmarks.ts
@@ -1,17 +1,27 @@
 import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.table('bookmarks', function (table) {
-    table.boolean('isdeleted').notNullable().defaultTo(false);
-  });
+  const exists = await knex.schema.hasTable('bookmarks');
+
+  if (exists) {
+    const hasColumn = await knex.schema.hasColumn('bookmarks', 'isdeleted');
+    if (!hasColumn) {
+      await knex.schema.table('bookmarks', function (table) {
+        table.boolean('isdeleted').notNullable().defaultTo(false);
+      });
+    }
+  }
 }
 
 export async function down(knex: Knex): Promise<void> {
-  const exits = await knex.schema.hasTable('bookmarks');
+  const exists = await knex.schema.hasTable('bookmarks');
 
-  if (exits) {
-    await knex.schema.table('bookmarks', function (table) {
-      table.dropColumn('isdeleted');
-    });
+  if (exists) {
+    const hasColumn = await knex.schema.hasColumn('bookmarks', 'isdeleted');
+    if (hasColumn) {
+      await knex.schema.table('bookmarks', function (table) {
+        table.dropColumn('isdeleted');
+      });
+    }
   }
 }
